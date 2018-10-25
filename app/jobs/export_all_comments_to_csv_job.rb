5c13dc69a1ee717a2d112db7141649fb195912fd
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-25 15:30:11 +0300

    add comments to csv job

--- app/jobs/export_all_comments_to_csv_job.rb
+++ app/jobs/export_all_comments_to_csv_job.rb
@@ -1,7 +1,14 @@
+require 'csv'
 class ExportAllCommentsToCsvJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    csv_string = CSV.generate do |csv|
+      csv << %w[author text post_id]
+      Comment.all.each do |comment|
+        csv << [comment[:author], comment[:text], comment[:post_id]]
+      end
+    end
+    puts csv_string
   end
 end
