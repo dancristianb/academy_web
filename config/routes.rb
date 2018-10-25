c183ea3e9a6c7416b63e7279c9a39214bf1e844d
Dan Cristian Bota <dan.cristian.bota@gmail.com>
2018-10-25 16:21:38 +0300

    add users route and root users#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :users
-  # resources :comments
   resources :posts do
     resources :comments
   end
+
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
